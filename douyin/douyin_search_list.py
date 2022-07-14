6f9ae013419d8570e9277b2800471a6fe7a5c75b
格陵兰（张海龙） <gelinglan.zhl@hhodata.tech>
2022-07-14 14:39:05 +0800

    fix

--- douyin/douyin_search_list.py
+++ douyin/douyin_search_list.py
@@ -1,20 +1,37 @@
+import time
+
 import uiautomator2 as u2
 
 
+# 仅适用于抖音18.5.0其他版本未测试xpath
+
 def gundam_script() -> bool:
-    d = u2.connect('{equipment}')
+    # d = u2.connect('{equipment}')
+    d = u2.connect("25cb279e")
     d.press("home")
-    d.app_start('com.zhiliaoapp.musically')
-    d.xpath('//*[@resource-id="com.zhiliaoapp.musically:id/exl"]/android.widget.ImageView[2]').click()
-    d.set_fastinput_ime(True)  #
-    d.send_keys('{word}')  # adb广播输入
-    d.set_fastinput_ime(False)  # 切换成正常的输入法
+    d.app_stop("com.ss.android.ugc.aweme")
+    d.app_start("com.ss.android.ugc.aweme")
+    with d.watch_context() as ctx:
+        ctx.when("^(DENY|拒绝)").when("(允许|ALLOW)").click()
+        ctx.when("同意").click()
+        ctx.when("确定").click()
+        ctx.wait_stable()
+    d(resourceId="com.ss.android.ugc.aweme:id/e5r").click()
+    d(focused=True).set_text("厨房神器")
     d.send_action("search")
-    d.app_stop('com.zhiliaoapp.musically')
+    d(resourceId="com.ss.android.ugc.aweme:id/de8").click()
+    d(resourceId="com.ss.android.ugc.aweme:id/ibv", text="最多点赞").click()
+    d(resourceId="com.ss.android.ugc.aweme:id/ibv", text="一天内").click()
+    d(resourceId="com.ss.android.ugc.aweme:id/de8").click()
+    d(resourceId="android:id/text1", text="视频").click()
+    count = 0
+    while count < 200:
+        count = count + 1
+        d.swipe(250, 1000, 250, 0, 0.01)
+        if d(text="暂时没有更多了").exists(timeout=0.01):
+            return True
     return True
 
 
 if __name__ == '__main__':
-    # apify_client = ApifyClient(token='token', api_url='localhost:8080')
-    # apify_client.key_value_store()
     gundam_script()
--- tiktok/list.py
+++ tiktok/list.py
@@ -1,18 +1,24 @@
+import time
 import uiautomator2 as u2
+from apify_client import ApifyClient
 
 
 def gundam_script() -> bool:
     d = u2.connect('{equipment}')
     d.press("home")
     d.app_start('com.zhiliaoapp.musically')
-    d.xpath('//*[@resource-id="com.zhiliaoapp.musically:id/exl"]/android.widget.ImageView[2]').click()
+    d.xpath('//*[@resource-id="com.zhiliaoapp.musically:id/exl"]/android.widget.ImageView[1]').click()
     d.set_fastinput_ime(True)  #
-    d.send_keys("")  # adb广播输入
+    d.send_keys("cat")  # adb广播输入
     d.set_fastinput_ime(False)  # 切换成正常的输入法
     d.send_action("search")
-    d.app_stop('com.zhiliaoapp.musically')
+    d(resourceId="com.zhiliaoapp.musically:id/cry").click()
+    d(resourceId="com.zhiliaoapp.musically:id/g1r", text="Yesterday").click()
+    d(resourceId="com.zhiliaoapp.musically:id/g1r", text="Most liked").click()
+    d(resourceId="com.zhiliaoapp.musically:id/dof").click()
+    d.swipe_ext("left")
+    d.swipe_ext("left")
     return True
-
 
 if __name__ == '__main__':
     # apify_client = ApifyClient(token='token', api_url='localhost:8080')
